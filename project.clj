cfcdd716e209d74c70690f13afdd2b8e256c0880
Grady Simon <grady@gradysimon.com>
2016-06-29 22:28:49 -0700

    Add setting color of circle via redis key

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject sparquil "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.taoensso/carmine "2.13.1"]
+                 [quil "2.4.0"]]
+  :repl-options {:init-ns sparquil.core})
---
+++ src/sparquil/core.clj
@@ -0,0 +1,53 @@
+(ns sparquil.core
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [taoensso.carmine :as car :refer (wcar)]))
+
+;; ---- Redis ----
+(def redis-conn {:pool {} :spec {:host "127.0.0.1" :port 6379}}) ; See `wcar` docstring for opts
+(defmacro wcar* [& body] `(car/wcar redis-conn ~@body))
+
+(defn setup []
+  ; Set frame rate to 30 frames per second.
+  (q/frame-rate 30)
+  ; Set color mode to HSB (HSV) instead of default RGB.
+  (q/color-mode :hsb)
+  ; setup function returns initial state. It contains
+  ; circle color and position.
+  {:color 0
+   :angle 0})
+
+(defn update-state [state]
+  ; Update sketch state by changing circle color and position.
+  {:color (or (Integer. (wcar* (car/get "env/color")))
+              (mod (+ (:color state) 0.7) 255))
+   :angle (+ (:angle state) 0.1)})
+
+(defn draw-state [state]
+  ; Clear the sketch by filling it with light-grey color.
+  (q/background 240)
+  ; Set circle color.
+  (q/fill (:color state) 255 255)
+  ; Calculate x and y coordinates of the circle.
+  (let [angle (:angle state)
+        x (* 150 (q/cos angle))
+        y (* 150 (q/sin angle))]
+    ; Move origin point to the center of the sketch.
+    (q/with-translation [(/ (q/width) 2)
+                         (/ (q/height) 2)]
+      ; Draw the circle.
+      (q/ellipse x y 100 100))))
+
+(q/defsketch sparquil
+  :title "You spin my circle right round"
+  :size [500 500]
+  ; setup function called only once, during sketch initialization.
+  :setup setup
+  ; update-state is called on each iteration before draw-state.
+  :update update-state
+  :draw draw-state
+  :features [:keep-on-top]
+  ; This sketch uses functional-mode middleware.
+  ; Check quil wiki for more info about middlewares and particularly
+  ; fun-mode.
+  :middleware [m/fun-mode])
