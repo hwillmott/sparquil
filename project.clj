81b62d4f2349637fd58c67b061a26d183d27b274
Grady Simon <grady@gradysimon.com>
2016-07-05 20:02:07 -0700

    Refactor to use Component
    Wrapped all global state in components connected by a system.

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.9.0-alpha8"]
+                 [org.tobereplaced/mapply "1.0.0"]
+                 [com.stuartsierra/component "0.3.1"]
                  [com.taoensso/carmine "2.13.1"]
                  [quil "2.4.0"]]
   :repl-options {:init-ns sparquil.core})
